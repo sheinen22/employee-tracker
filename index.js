df5b311a3507c81cb6026310f41d1ab6806397ec
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-29 18:01:37 -0500

    added all of the questions, just need functions, big if/else statement, to prompt questions

--- index.js
+++ index.js
@@ -9,3 +9,76 @@ const jobTitles = ["President", 'Vice President', 'Operations Manager', 'Sales R
 
 const employees = ['Wade', 'Matt', 'Sam', 'Al', 'Niki', 'Danny', 'John', 'Jason', 'Juan', 'Stephen', 'Lalo', 'Adrian']
 
+//everything needs a separate definition.  literally everything
+
+const options = [
+    {
+      type: 'list',
+      message: "What would you like to do?",
+      choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+      name: 'options',
+   },
+]; 
+
+const addDeptName = [
+    {
+        message: "What is the new department name?",
+        name: "addDeptName"
+    },
+];
+
+const addJobTitle = [
+    {
+      message: 'What is the name of the role?',
+      name: 'addJobTitle',
+    },
+    {
+        message: 'What is the salary of the role?',
+        name: 'newSalary',
+    },
+    {
+        type: 'list',
+        message: "What department does the role belong to?",
+        choices: departments,
+        name: 'roleDepartment',
+     },
+    
+]; 
+
+const addEmployee = [
+    {
+      message: 'What is the employees first name?',
+      name: 'empFirstName',
+    },
+    {
+      message: 'What is the employees last name?',
+      name: 'empLastName',
+    },
+    {
+      type: 'list',
+      message: "What is the employees job title?",
+      choices: jobTitles,
+      name: 'empJobTitle',
+     },
+     {
+        type: 'list',
+        message: "Who is the employees manager?",
+        choices: employees,
+        name: 'empManager'
+     }
+]
+
+const updateEmployee = [
+    {
+        type: 'list',
+        message: 'Which employee would you like to update?',
+        choices: employees,
+        name: "updateEmployee"
+    }
+    {
+        type: 'list',
+        message: 'What is the new title of this employee?',
+        choices: jobTitles,
+        name: "updateEmployeeTitle"
+    }
+]
